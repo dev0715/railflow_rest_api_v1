f98d33bc733d571bc7d7a8277fc68e428d0419f3
dev0715 <toptal126@outlook.com>
2021-01-24 16:36:53 +0530

    Add sample tests

---
+++ tests/integration/sample.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+describe("RGB to Hex conversion", function () {
+    it("converts the basic colors", function () {
+        expect(1 + 1).to.equal(2);
+    });
+});

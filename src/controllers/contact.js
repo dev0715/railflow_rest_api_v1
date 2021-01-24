bfa0e31b32403d554781e7f2b0a36345055672aa
dev0715 <toptal126@outlook.com>
2021-01-24 11:38:42 +0530

    Initialize the project. Add signup api

---
+++ src/controllers/contact.js
@@ -0,0 +1,71 @@
+/**
+ * User controller.
+ * Contains all the business logic executed after
+ * hitting any user endpoint in routes.
+ */
+
+"use strict";
+
+// const ApiError = require("../errors/api");
+const UnprocessableRequestError = require("../errors/unprocessablerequest");
+// const BadRequestError = require("../errors/badrequest");
+
+const contactService = require('../services/contact');
+const accountService = require('../services/account');
+
+async function createContact(request, res, next) {
+  try {
+    const data = {
+      firstName: request.body.firstName,
+      lastName: request.body.lastName,
+      email: request.body.email,
+      phone: request.body.phone,
+      jobTitle: request.body.jobTitle,
+      company: request.body.company,
+    };
+
+    // check if the contact is already there.
+    const alreadyPresent = await contactService.checkIfAlreadyPresent(request.body.email);
+    if (alreadyPresent) {
+      throw new UnprocessableRequestError(`Whoops. It seems that you have already evaluated Railflow. If you would like to evaluate again or would like to extend your license, please go to  and submit a ticket. Someone from our customer success team will help you right away. You can also leave a message in our chat bot and it will also notify the customer success team.`);
+    }
+
+    const resp = await accountService.create({ name: request.body.company });
+
+    if (resp.status == 200) {
+      data.sales_accounts = [{
+        id: resp.data.sales_account.id,
+        is_primary: true,
+      }];
+
+      const response = await contactService.create(data);
+      if (response && response.data && response.data.contact) {
+        console.log(`> contactService:create`);
+        // const notificationData = {
+        //   contactId: response.data.contact.id,
+        //   company: request.body.company,
+        // };
+
+        // await this.slackService.notifyContactCreation(notificationData);
+        console.log(`> sending response contact create`);
+        return res.status(response.status).send({
+          status: response.status,
+          data: response.data
+        });
+      }
+    }
+
+    return res.status(500).send({
+      status: 500,
+      data: {
+        message: 'Something went wrong.'
+      },
+    });
+  } catch (error) {
+    return res.status(error.status).send(error.toJSON());
+  }
+}
+
+module.exports = {
+  createContact,
+};
---
+++ src/services/contact.js
@@ -0,0 +1,112 @@
+/**
+ * User controller.
+ * Contains all the business logic executed after
+ * hitting any user endpoint in routes.
+ */
+
+"use strict";
+
+const { createAccount } = require("../services/account");
+const { getApiClient } = require('../services/request');
+
+const appConfigs = require('../../configs/app');
+const configs = appConfigs.getConfigs(process.env.APP_ENV || "development");
+
+const ApiError = require("../errors/api");
+// const UnprocessableRequestError = require("../errors/unprocessablerequest");
+// const BadRequestError = require("../errors/badrequest");
+
+async function create(data) {
+  try {
+    const apiClient = await getApiClient(configs.FRESHSALES_BASE_URL);
+    const response = await apiClient.request({
+      method: 'POST',
+      url: '/crm/sales/api/contacts',
+      headers: {
+        // TODO: use environment variable
+        // Authorization: `Token token=${process.env.FRESHSALES_API_KEY}`,
+        Authorization: `Token token=${configs.FRESHSALES_API_KEY}`,
+      },
+      data: {
+        first_name: data.firstName,
+        last_name: data.lastName,
+        email: data.email,
+        mobile_number: data.phone,
+        job_title: data.jobTitle,
+        custom_field: {
+          cf_company: data.company,
+          cf_extension_period: 14, // setting this 14 as default. sales agent can change this number before firing license extension webhook.
+        },
+        sales_accounts: data.sales_accounts
+      },
+    });
+
+    return response;
+  } catch (error) {
+    console.log(error);
+    throw new ApiError(error);
+  }
+}
+
+async function checkIfAlreadyPresent(email) {
+  console.log(`> checkIfAlreadyPresent: ${email}`);
+  const apiClient = await getApiClient(configs.FRESHSALES_BASE_URL);
+  const response = await apiClient.request({
+    method: 'GET',
+    url: '/crm/sales/api/contacts/filters',
+    headers: {
+      // TODO: use environment variable
+      // Authorization: `Token token=${process.env.FRESHSALES_API_KEY}`,
+      Authorization: `Token token=${configs.FRESHSALES_API_KEY}`,
+    },
+  });
+
+  if (response && response.status === 200) {
+    console.log(`> checkIfAlreadyPresent:200`);
+    const filters = response.data.filters;
+    let viewId;
+    for (let f of filters) {
+      if (f.name === "All Contacts") {
+        viewId = f.id;
+      }
+    }
+
+    const allContacts = await getAllContactsFromView(viewId);
+
+    if (allContacts != null) {
+      for (let c of allContacts) {
+        if (c.email === email) {
+          console.log(`> checkIfAlreadyPresent:true`);
+          return true
+        }
+      }
+    }
+  }
+
+  console.log(`> checkIfAlreadyPresent:false`);
+  return false;
+}
+
+async function getAllContactsFromView(viewId) {
+  const apiClient = await getApiClient(configs.FRESHSALES_BASE_URL);
+  const response = await apiClient.request({
+    method: 'GET',
+    url: `/crm/sales/api/contacts/view/${viewId}`,
+    headers: {
+      // TODO: use environment variable
+      // Authorization: `Token token=${process.env.FRESHSALES_API_KEY}`,
+      Authorization: `Token token=${configs.FRESHSALES_API_KEY}`,
+    },
+  });
+
+  if (response && response.status === 200) {
+    return response.data.contacts;
+  }
+
+  return null;
+}
+
+module.exports = {
+  create,
+  checkIfAlreadyPresent,
+};

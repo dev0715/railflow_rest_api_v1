73c5bcd15f336954066478d842b6d02079d93f60
dev0715 <toptal126@outlook.com>
2021-01-30 21:06:37 +0530

    Add allowed domains

--- configs/app.js
+++ configs/app.js
@@ -3,18 +3,13 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-const {
-  APP_PORT,
-  ALLOWED_DOMAINS,
-} = process.env;
-
 const configs = {
     development: {
         // hard code these in this object
         APP_PORT: 9000,
         FRESHSALES_API_KEY: "fPjGQStTY1ffGqtyAj9RVw",
         FRESHSALES_BASE_URL: "https://railflow.myfreshworks.com",
-        ALLOWED_DOMAINS: "*",
+        ALLOWED_DOMAINS: ["http://localhost:8000"],
         CRYPTOLENS_BASE_URL: "https://app.cryptolens.io",
         CRYPTOLENS_API_KEY: "WyIyNjQ5MDgiLCIzT3ZBYlNybjRjSll1T0U1OU1jTUlydE5BSW90Ti93YWpod2d3TDVRIl0=",
         MAILGUN_KEY: "key-4c45b28d4cab5d63a74df82c99aae76c",

0d3e847779a5742d1c415c874954b6fcffb8b862
dev0715 <toptal126@outlook.com>
2021-02-01 22:20:07 +0530

    Allow railflow.io in cors

--- configs/app.js
+++ configs/app.js
@@ -9,7 +9,7 @@ const configs = {
         APP_PORT: 9000,
         FRESHSALES_API_KEY: "fPjGQStTY1ffGqtyAj9RVw",
         FRESHSALES_BASE_URL: "https://railflow.myfreshworks.com",
-        ALLOWED_DOMAINS: ["http://localhost:8000"],
+        ALLOWED_DOMAINS: ["http://localhost:8000", "https://railflow.io"],
         CRYPTOLENS_BASE_URL: "https://app.cryptolens.io",
         CRYPTOLENS_API_KEY: "WyIyNjQ5MDgiLCIzT3ZBYlNybjRjSll1T0U1OU1jTUlydE5BSW90Ti93YWpod2d3TDVRIl0=",
         MAILGUN_KEY: "key-4c45b28d4cab5d63a74df82c99aae76c",

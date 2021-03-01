543d307055eabd361c4a7b3314fced5e7c3933ab
dev0715 <lavelle.warren.1995@gmail.com>
2021-03-01 23:28:58 +0530

    Fix contact api

--- src/services/account.js
+++ src/services/account.js
@@ -30,7 +30,7 @@ async function create(data) {
         });
 
         console.log(`> account created with name: ${data.name}`);
-        return response;
+        return response.data.sales_account;
     } catch (error) {
         if (error.response.data.errors.code === 400) {
             throw new BadRequestError(`Account with given company name already exists.`);
@@ -39,6 +39,62 @@ async function create(data) {
     }
 }
 
+async function getAccountIfAlreadyPresent(name) {
+    const apiClient = await getApiClient(configs.FRESHSALES_BASE_URL);
+    const response = await apiClient.request({
+        method: 'GET',
+        url: '/crm/sales/api/sales_accounts/filters',
+        headers: {
+            // TODO: use environment variable
+            // Authorization: `Token token=${process.env.FRESHSALES_API_KEY}`,
+            Authorization: `Token token=${configs.FRESHSALES_API_KEY}`,
+        },
+    });
+
+    if (response && response.status === 200) {
+        const filters = response.data.filters;
+        let viewId;
+        for (let f of filters) {
+            if (f.name === "All Accounts") {
+                viewId = f.id;
+            }
+        }
+
+        const allAccounts = await getAllAccountsFromView(viewId);
+
+        if (allAccounts != null) {
+            for (let a of allAccounts) {
+                if (a.name === name) {
+                    return a;
+                }
+            }
+        }
+    }
+
+    return null;
+}
+
+
+async function getAllAccountsFromView(viewId) {
+    const apiClient = await getApiClient(configs.FRESHSALES_BASE_URL);
+    const response = await apiClient.request({
+        method: 'GET',
+        url: `/crm/sales/api/sales_accounts/view/${viewId}`,
+        headers: {
+            // TODO: use environment variable
+            // Authorization: `Token token=${process.env.FRESHSALES_API_KEY}`,
+            Authorization: `Token token=${configs.FRESHSALES_API_KEY}`,
+        },
+    });
+
+    if (response && response.status === 200) {
+        return response.data.sales_accounts;
+    }
+
+    return null;
+}
+
 module.exports = {
     create,
+    getAccountIfAlreadyPresent,
 };

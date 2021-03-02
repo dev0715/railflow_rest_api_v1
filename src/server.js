9991e3fc5d01f8cb8d014a1722c2c2e55d492f9b
dev0715 <toptal126@outlook.com>
2021-03-02 15:26:44 +0700

    init swagger

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const contactRouter = require("./routes/contact");
 const signupRouter = require("./routes/signup");
 const licenseRouter = require("./routes/license");
@@ -16,9 +18,46 @@ const config = appConfig.getConfigs(process.env.APP_ENV || "development");
 
 class Server {
   constructor() {
+    const swaggerDefinition = {
+      openapi: '3.0.0',
+      info: {
+        title: 'Express API for Railflow',
+        version: '1.0.0',
+        description:
+          'This is a REST API application made with Express. It retrieves data from Railflow.',
+        license: {
+          name: 'Licensed Under MIT',
+          url: 'https://spdx.org/licenses/MIT.html',
+        },
+        contact: {
+          name: 'Railflow',
+          url: 'https://railflow.io',
+        },
+      },
+      servers: [
+        {
+          url: 'http://localhost:9000',
+          description: 'Development server',
+        },
+        {
+          url: 'http://api.railflow.io',
+          description: 'Production server',
+        },
+      ],
+    };
+
+    const options = {
+      swaggerDefinition,
+      // Paths to files containing OpenAPI definitions
+      // apis: ['src/routes/*.js'],
+      apis: ['src/swagger.js','src/routes/*.js']
+    };
+
+    const swaggerSpec = swaggerJSDoc(options);
     this.app = express();
     this.config();
     this.routes();
+    this.app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
   }
 
   config() {

766bd7f8d5a2e2a6fe1a057425a3a27d7850469d
dev0715 <toptal126@outlook.com>
2021-04-22 23:18:16 +0700

    Added the slack slash command

--- src/controllers/license.js
+++ src/controllers/license.js
@@ -7,10 +7,12 @@
 "use strict";
 
 const ApiError = require("../errors/api");
+const { getApiClient } = require('../services/request');
 
 const emailService = require('../services/email');
 const noteService = require('../services/note');
 const licenseService = require('../services/license');
+const contactService = require('../services/contact');
 const { checkToken, checkTokenSlash } = require('../services/token');
 
 /**
@@ -77,26 +79,39 @@ async function extendLicense(req, res, next) {
         const itemsplited = item.split(":");
         req.body[itemsplited[0]]=itemsplited[1];
       });
-      
-    // res.json({"text":"`contact_id` ⎵ `contact_name` ⎵ `email`"});
-    res.json({resData:req.body});
-    return res.status(200).send({
-      status: 200,
-      message: 'token is valid'
-    });
-  
+    if (typeof req.body.license_key !== 'undefined') {
+      req.body.contact_cf_license_key=req.body.license_key;
+    } else {
+      res.json({"text":"License Key is invalid, please follow this example (⎵ is whitespace): `contact_id:16004191559⎵extension_period:8⎵license_key:ICWUF-JHARN-GEGRI-XDMYN`"});
+    }
+    if (parseInt(req.body.extension_period)) {
+      req.body.contact_cf_extension_period = parseInt(req.body.extension_period);
+    }
+    else {
+      res.json({"text":"Period is invalid, please follow this example (⎵ is whitespace): `contact_id:16004191559⎵extension_period:8⎵license_key:ICWUF-JHARN-GEGRI-XDMYN`"});
+    }
+    console.log(req.body);
+    const apiClient = await getApiClient(req.body.response_url);
     try {
+        const contact = await contactService.getContactById(req.body.contact_id)
+        // Return HTTP 200 to Slack
+        res.json({
+          "response_type": "in_channel",
+          "text":"Extending the license..."
+        });
         const license = await licenseService.extend(req.body);
         const description = `License has been extended by ${req.body.contact_cf_extension_period} days`;
-        const createNotesResponse = await noteService.create(req.body.contact_id, description);
-        await sendLicenseExtensionEmail(req.body, `Your license has been extended by ${req.body.contact_cf_extension_period} days.`);
-
-        return res.status(200).send({
-            status: 200,
+        const createNotesResponse = await noteService.create(req.body.contact_id, description, true);
+        req.body.contact_email = contact.email;
+        sendLicenseExtensionEmail(req.body, `Your license has been extended by ${req.body.contact_cf_extension_period} days.`);
+        await apiClient.request({
+            method: 'POST',
             data: {
-                message: `license extended successfully for the contact: ${req.body.contact_id}`,
+              replace_original: true,
+              text: `Extended the license, updated notes, onboarding email sent.`
             }
         });
+        
     } catch (error) {
         console.log(`> error while extending license for: ${req.body.contact_id}: ${error}`);
         return res.status(error.status).send(error.toJSON());

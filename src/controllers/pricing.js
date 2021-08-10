50354500ffa60b790ae9a7374f0ebe5d6d75ed3d
dev0715 <lavelle.warren.1995@gmail.com>
2021-08-10 23:36:09 +0700

    GAB-90 pricing new response

--- src/controllers/pricing.js
+++ src/controllers/pricing.js
@@ -25,7 +25,7 @@ async function getPricing(request, res, next) {
             message: "token invalid or missing",
         });
     }
-    if (typeof(request.query.license_type) === "undefined" || typeof(request.query.license_years) === "undefined" || typeof(request.query.num_users) === "undefined") {
+    if (typeof(request.query.license_type) === "undefined") {
         let tiers = [];
         for (let index = 0; index < users.list_size; index++) {
             tiers.push(`${index * users.increment} - ${(index + 1) * users.increment}`);
@@ -45,29 +45,12 @@ async function getPricing(request, res, next) {
         });
     }
 
-    if (isNaN(request.query.num_users) || request.query.num_users < 0 || request.query.num_users > 49) {
-        return res.status(400).send({
-            status: 400,
-            data: {
-                message: 'Invalid num_users: valid value is: 0-49'
-            }
-        });
-    }
-
-    if (isNaN(request.query.license_years) || request.query.license_years < 0 || request.query.license_years > 3) {
-        return res.status(400).send({
-            status: 400,
-            data: {
-                message: 'Invalid License_years: valid values are 0-3'
-            }
-        });
-    }
     let resData = {};
-    let price_option = 0
+    let price_options = [];
     let discount_amt = 0;
-    if (request.query.num_users != null) {
-        price_option = request.query.num_users >> 0;
-    } 
+    for (let index = 0; index < users.list_size; index++) {
+        price_options.push(index >> 0)
+    }
     const pricingType = pricing[request.query.license_type];
     if (pricingType == undefined || pricingType == null) {
         return res.status(400).send({
@@ -76,28 +59,36 @@ async function getPricing(request, res, next) {
             }
         });
     }
-    resData.license_type = request.query.license_type;
-    resData.base = pricingType.base;
-    resData.increment = pricingType.increment;
-    resData.users = `${20*price_option}-${20*(price_option+1)}`;
-    resData.num_users = request.query.num_users;
-    resData.base_price = pricingType.base + (pricingType.increment * price_option);
-    resData.years = request.query.license_years;
-    if (request.query.license_years == 0) {
-        resData.total_price = resData.base_price * 4;
-        resData.discount_rate = pricingType.discount_perpetual;
-    } else {
-        resData.total_price = resData.base_price * request.query.license_years;
-        resData.discount_rate = pricingType[`discount_${request.query.license_years}_year`];
+    for (let index = 0; index <= pricingType.years; index++) {
+        resData.license_type = request.query.license_type;
+        resData.base = pricingType.base;
+        resData.increment = pricingType.increment;
+        let licenseName = `${index} Year License`;
+        let licenseYear = index;
+        if (index == 0) {
+            licenseName = `Perpetual License`;
+            licenseYear = 4;
+            resData[licenseName] = {};
+            resData[licenseName].discount_rate = pricingType.discount_perpetual;
+        } else {
+            resData[licenseName] = {};
+            resData[licenseName].discount_rate = pricingType[`discount_${index}_year`];
+        }
+        resData[licenseName].tiers = [];
+        price_options.forEach(price_option => {
+            let tier = {};
+            tier.users = `${20*price_option}-${20*(price_option+1)}`;
+            tier.yearly_price = pricingType.base + (pricingType.increment * price_option);
+            tier.undiscount_price = tier.yearly_price * licenseYear;
+            discount_amt = tier.undiscount_price * resData[licenseName].discount_rate;
+            tier.discount = Math.round(discount_amt*100)/100;
+            tier.final_price = tier.undiscount_price - tier.discount;
+            resData[licenseName].tiers.push(tier);
+        });
     }
-    if (resData.discount_rate) {
-        discount_amt = resData.total_price * resData.discount_rate;
-        resData.discount_amt = Math.round(discount_amt*100)/100;
-    }
-    resData.final_price = resData.total_price - Math.round(discount_amt*100)/100;
     return res.status(200).send({
-        status: 200,
-        data:resData
+        message: "Pricing detail",
+        pricing:resData
     });
 }
 

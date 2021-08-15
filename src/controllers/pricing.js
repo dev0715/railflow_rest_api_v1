50d14af225946bf3069fb753ae9da5c42604eff6
dev0715 <toptal126@outlook.com>
2021-08-16 06:44:30 +0700

    GAB-49 adding user config to pricing

--- src/controllers/pricing.js
+++ src/controllers/pricing.js
@@ -25,7 +25,7 @@ async function getPricing(request, res, next) {
             message: "token invalid or missing",
         });
     }
-    if (typeof(request.query.license_type) === "undefined") {
+    if (typeof request.query.license_type === "undefined") {
         let tiers = [];
         for (let index = 0; index < users.list_size; index++) {
             tiers.push(`${index * users.increment} - ${(index + 1) * users.increment}`);
@@ -33,15 +33,77 @@ async function getPricing(request, res, next) {
         let resPrices = {};
         for (const [key, value] of Object.entries(pricing)) {
             resPrices[key] = {
-                base: value.base
+                base: value.base,
             };
-          }
+        }
         return res.status(200).send({
             message: "Only base price",
-            users:{
-                tiers: tiers
+            users: {
+                tiers: tiers,
             },
             pricing: resPrices,
+        });
+    }
+    const data = {
+        account_id: request.query.account_id,
+        contact_id: request.query.contact_id,
+        num_users: request.query.num_users,
+        license_type: request.query.license_type,
+        license_years: request.query.license_years,
+    };
+    if (typeof data.license_years !== 'undefined' && typeof data.num_users !== 'undefined') {
+        if (isNaN(data.num_users) || data.num_users < 0 || data.num_users > 49) {
+            return res.status(400).send({
+                status: 400,
+                data: {
+                    message: 'Invalid num_users: valid value is: 0-49'
+                }
+            });
+        }
+        if (isNaN(data.license_years) || data.license_years < 0 || data.license_years > 3) {
+            return res.status(400).send({
+                status: 400,
+                data: {
+                    message: 'Invalid License_years: valid values are 0-3'
+                }
+            });
+        }
+        let resData = {};
+        resData.license_type = data.license_type;
+        let price_option = 0
+        if (request.query.num_users != null) {
+            price_option = request.query.num_users >> 0;
+        } 
+        let discount_amt = 0;
+        const pricingType = pricing[request.query.license_type];
+        if (pricingType == undefined || pricingType == null) {
+            return res.status(400).send({
+                data: {
+                    message: "Incorrect value for license_type",
+                },
+            });
+        }
+        resData.base = pricingType.base;
+        resData.increment = pricingType.increment;
+        resData.users = `${20*price_option}-${20*(price_option+1)}`;
+        resData.num_users = request.query.num_users;
+        resData.base_price = pricingType.base + (pricingType.increment * price_option);
+        resData.years = request.query.license_years;
+        if (request.query.license_years == 0) {
+            resData.total_price = resData.base_price * 4;
+            resData.discount_rate = pricingType.discount_perpetual;
+        } else {
+            resData.total_price = resData.base_price * request.query.license_years;
+            resData.discount_rate = pricingType[`discount_${request.query.license_years}_year`];
+        }
+        if (resData.discount_rate) {
+            discount_amt = resData.total_price * resData.discount_rate;
+            resData.discount_amt = Math.round(discount_amt*100)/100;
+        }
+        resData.final_price = resData.total_price - Math.round(discount_amt*100)/100;
+        return res.status(200).send({
+            message: `Pricing detail`,
+            pricing: resData
         });
     }
 
@@ -49,14 +111,14 @@ async function getPricing(request, res, next) {
     let price_options = [];
     let discount_amt = 0;
     for (let index = 0; index < users.list_size; index++) {
-        price_options.push(index >> 0)
+        price_options.push(index >> 0);
     }
     const pricingType = pricing[request.query.license_type];
     if (pricingType == undefined || pricingType == null) {
         return res.status(400).send({
             data: {
-                message: "Incorrect value for license_type"
-            }
+                message: "Incorrect value for license_type",
+            },
         });
     }
     for (let index = 0; index <= pricingType.years; index++) {
@@ -75,20 +137,20 @@ async function getPricing(request, res, next) {
             resData[licenseName].discount_rate = pricingType[`discount_${index}_year`];
         }
         resData[licenseName].tiers = [];
-        price_options.forEach(price_option => {
+        price_options.forEach((price_option) => {
             let tier = {};
-            tier.users = `${20*price_option}-${20*(price_option+1)}`;
-            tier.yearly_price = pricingType.base + (pricingType.increment * price_option);
+            tier.users = `${20 * price_option}-${20 * (price_option + 1)}`;
+            tier.yearly_price = pricingType.base + pricingType.increment * price_option;
             tier.undiscount_price = tier.yearly_price * licenseYear;
             discount_amt = tier.undiscount_price * resData[licenseName].discount_rate;
-            tier.discount = Math.round(discount_amt*100)/100;
+            tier.discount = Math.round(discount_amt * 100) / 100;
             tier.final_price = tier.undiscount_price - tier.discount;
             resData[licenseName].tiers.push(tier);
         });
     }
     return res.status(200).send({
         message: "Pricing detail",
-        pricing:resData
+        pricing: resData,
     });
 }
 

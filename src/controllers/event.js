565c5248c40fa6b19e7eb43b41115bc2c49f9464
dev0715 <lavelle.warren.1995@gmail.com>
2021-09-29 22:13:37 +0200

    some validation in event

--- src/controllers/event.js
+++ src/controllers/event.js
@@ -6,11 +6,11 @@
 
 "use strict";
 
-const dayjs = require('dayjs');
+const dayjs = require("dayjs");
 
 const BadRequestError = require("../errors/badrequest");
-const noteService = require('../services/note');
-const { checkToken } = require('../services/token');
+const noteService = require("../services/note");
+const { checkToken } = require("../services/token");
 
 /**
  * Function: Create new Event
@@ -20,38 +20,53 @@ const { checkToken } = require('../services/token');
  * @returns Promise
  */
 async function createEvent(req, res, next) {
-    // Middleware: Check token beforehand
-    const isAuthenticated = await checkToken(req.headers.token);
-    if (!isAuthenticated) {
-      return res.status(400).send({
-        status: 400,
-        message: 'token invalid or missing'
-      });
-    }
-  
-    try {
-        if (!(req.body["event-data"]["user-variables"] && req.body["event-data"]["user-variables"].contactId)) {
-            throw new BadRequestError(`Contact id is required to register event`);
-        }
+  // Middleware: Check token beforehand
+  const isAuthenticated = await checkToken(req.headers.token);
+  if (!isAuthenticated) {
+    return res.status(400).send({
+      status: 400,
+      message: "token invalid or missing",
+    });
+  }
 
-        const eventData = req.body["event-data"];
-        const event = eventData.event;
-        const contactId = eventData["user-variables"].contactId;
-        const timestamp = req.body.signature.timestamp * 1000; // converting to mili seconds
-        let noteDescription = `Email was: ${event} at ${dayjs(timestamp).format('YYYY-MM-DD HH:mm:ss')}`;
-        await noteService.create(contactId, noteDescription)
-        return res.status(200).send({
-            status: 200,
-            data: {
-                message: `Event created for contact id: ${contactId}`
-            }
-        });
-    } catch (error) {
-        console.log(`> error while creating event for: ${req.body["event-data"]["user-variables"].contactId}: ${error}`);
-        return res.status(error.status).send(error.toJSON());
+  try {
+    if (
+      !(
+        req.body["event-data"]["user-variables"] &&
+        req.body["event-data"]["user-variables"].contactId
+      )
+    ) {
+      throw new BadRequestError(`Contact id is required to register event`);
     }
+    if (!req.body.signature || !req.body.signature.timestamp) {
+      throw new BadRequestError(`signature.timestamp required to register event`);
+    }
+    if (!req.body["event-data"] || !req.body["event-data"].event) {
+      throw new BadRequestError(`event required to register event`);
+    }
+
+    const eventData = req.body["event-data"];
+    const event = eventData.event;
+    const contactId = eventData["user-variables"].contactId;
+    const timestamp = req.body.signature.timestamp * 1000; // converting to mili seconds
+    let noteDescription = `Email was: ${event} at ${dayjs(timestamp).format(
+      "YYYY-MM-DD HH:mm:ss"
+    )}`;
+    await noteService.create(contactId, noteDescription);
+    return res.status(200).send({
+      status: 200,
+      data: {
+        message: `Event created for contact id: ${contactId}`,
+      },
+    });
+  } catch (error) {
+    console.log(
+      `> error while creating event for: ${req.body["event-data"]["user-variables"].contactId}: ${error}`
+    );
+    return res.status(error.status).send(error.toJSON());
+  }
 }
 
 module.exports = {
-    createEvent,
+  createEvent,
 };

493fdcd931c7f9a858a880e9bb41b4eb116e453f
dev0715 <toptal126@outlook.com>
2021-10-04 14:12:19 +0200

    google keys for logging added

--- src/services/upload.js
+++ src/services/upload.js
@@ -12,7 +12,7 @@ const logger = require("../config/logger");
 
 // The name of the bucket that you have created
 const storage = new Storage({
-  keyFilename: "railflow-gcp-prod-2.json",
+  keyFilename: "railflow-gcp-prod.json",
   projectId: "railflow-production",
 });
 

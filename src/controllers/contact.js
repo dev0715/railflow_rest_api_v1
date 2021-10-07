c8b54f5ec779d7eb64a52ab6c4d810e63dd048e2
dev0715 <toptal126@outlook.com>
2021-10-07 02:22:44 +0200

    store the phone in the work_number field

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -46,7 +46,7 @@ async function createContact(request, res, next) {
       firstName: request.body.firstName,
       lastName: request.body.lastName,
       email: request.body.email,
-      phone: request.body.phone,
+      work_number: request.body.phone,
       jobTitle: request.body.jobTitle,
       company: request.body.company,
     };

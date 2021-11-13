47acecb3f0e8c18a805f6017f953c334759f6c0e
dev0715 <toptal126@outlook.com>
2021-11-13 14:40:05 -0800

    migration to render

--- configs/app.js
+++ configs/app.js
@@ -14,7 +14,9 @@ const configs = {
     "http://localhost:9000",
     "https://railflow.io",
     "https://www.railflow.io",
-    "https://railflow-website.onrender.com",
+    "https://railflow.myfreshworks.com",
+    "https://api.mailgun.net/",
+
   ],
   CRYPTOLENS_BASE_URL: "https://app.cryptolens.io",
   CRYPTOLENS_API_KEY: "WyI1NTkwNTYzIiwidE1WQ1NjaUlwR05adHhZK1BaaEZlamVpUEZ5RElJQ3ZBZGlPVzFkUCJd",

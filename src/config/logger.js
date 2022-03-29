6e488694aa957649955a1ab6fa92397e05297bcb
dev0715 <toptal126@outlook.com>
2022-03-29 14:39:15 +0200

    logz.io added

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,31 +1,45 @@
-const winston = require("winston");
-const { Logtail } = require("@logtail/node");
-const { LogtailTransport } = require("@logtail/winston");
+const winston = require('winston')
+// const { Logtail } = require('@logtail/node')
+// const { LogtailTransport } = require('@logtail/winston')
 
-// Create a Logtail client
-const logtail = new Logtail("xTkwgvG9woJf1ciDrYup27FR");
+// // Create a Logtail client
+// const logtail = new Logtail('xTkwgvG9woJf1ciDrYup27FR')
 
-// Create a Winston logger - passing in the Logtail transport
-const logger_live = winston.createLogger({
-  transports: [new LogtailTransport(logtail)],
-});
+// // Create a Winston logger - passing in the Logtail transport
+// const logger_live = winston.createLogger({
+//   transports: [new LogtailTransport(logtail)],
+// })
+
+// const logger = winston.createLogger({
+//   //   level: "info",
+//   colorize: true,
+//   transports: [
+//     new winston.transports.Console({
+//       level: 'verbose',
+//       format: winston.format.combine(
+//         // winston.format.timestamp(),
+//         winston.format.prettyPrint(),
+//         winston.format.colorize(),
+//         winston.format.simple()
+//       ),
+//     }),
+//   ],
+// })
+
+const LogzioWinstonTransport = require('winston-logzio')
+
+const logzioWinstonTransport = new LogzioWinstonTransport({
+  level: 'info',
+  name: 'winston_logzio',
+  token: 'ynrwSYxoEcmqrsKGUWaZhVewrPiUtMYg',
+  host: 'listener.logz.io',
+  port: '8071',
+  protocol: 'https',
+})
 
 const logger = winston.createLogger({
-  //   level: "info",
-  colorize: true,
-  transports: [
-    new winston.transports.Console({
-      level: "verbose",
-      format: winston.format.combine(
-        // winston.format.timestamp(),
-        winston.format.prettyPrint(),
-        winston.format.colorize(),
-        winston.format.simple()
-      ),
-    }),
-  ],
-});
+  format: winston.format.simple(),
+  transports: [logzioWinstonTransport],
+})
 
-module.exports = logger;
-
-
+module.exports = logger
